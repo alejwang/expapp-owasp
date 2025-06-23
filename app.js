0bc5bdb98d362e5e37735c6d6819804737c1c25f
Zander Wang <me@zander.wang>
2025-06-22 19:22:13 -0700

    finish owasp

---
+++ app.js
@@ -0,0 +1,113 @@
+const express               =  require('express'),
+      expSession            =  require("express-session"),
+      app                   =  express(),
+      mongoose              =  require("mongoose"),
+      passport              =  require("passport"),
+      bodyParser            =  require("body-parser"),
+      LocalStrategy         =  require("passport-local"),
+      passportLocalMongoose =  require("passport-local-mongoose"),
+      User                  =  require("./models/user"),
+      rateLimit             =  require("express-rate-limit"),
+      xss                   =  require("xss-clean"),
+      helmet                =  require("helmet"),
+      mongoSanitize         =  require("express-mongo-sanitize")
+
+//Connecting database
+mongoose.connect("mongodb://localhost/auth_demo");
+
+app.use(expSession({
+    secret:"mysecret",       //decode or encode session
+    resave: false,          
+    saveUninitialized: true,
+    cookie: {
+        secure: true,
+        httpOnly: true,
+        maxAge: 1 * 60 * 1000 // 1 day in milliseconds
+    } // set to true if using https
+}))
+
+passport.serializeUser(User.serializeUser());       //session encoding
+passport.deserializeUser(User.deserializeUser());   //session decoding
+passport.use(new LocalStrategy(User.authenticate()));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded(
+      { extended:true }
+))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static("public"));
+
+
+//=======================
+//      O W A S P
+//=======================
+
+app.use(mongoSanitize());
+
+const limit = rateLimit({
+    windowMs: 60 * 60 * 1000, // 60 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: "Too many requests"
+});
+
+app.use('/routeName', limit); // Apply rate limiting to specific routes
+
+app.use(express.json({limit: '10kb'})); // Limit JSON payload size to 10kb
+
+app.use(xss()); // Sanitize user input to prevent XSS attacks
+
+app.use(helmet()); // Set security headers
+
+//=======================
+//      R O U T E S
+//=======================
+app.get("/", (req,res) =>{
+    res.render("home");
+})
+app.get("/userprofile" ,(req,res) =>{
+    res.render("userprofile");
+})
+//Auth Routes
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+app.post("/login",passport.authenticate("local",{
+    successRedirect:"/userprofile",
+    failureRedirect:"/login"
+}),function (req, res){
+});
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
+
+app.post("/register",(req,res)=>{
+    
+    User.register(new User({username: req.body.username,email: req.body.email,phone: req.body.phone}),req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.render("register");
+        }
+        passport.authenticate("local")(req,res,function(){
+            res.redirect("/login");
+        })    
+    })
+})
+app.get("/logout",(req,res)=>{
+    req.logout();
+    res.redirect("/");
+});
+function isLoggedIn(req,res,next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+//Listen On Server
+app.listen(process.env.PORT || 3000,function (err) {
+    if(err){
+        console.log(err);
+    }else {
+        console.log("Server Started At Port 3000");  
+    }
+});
